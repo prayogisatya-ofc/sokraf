4f0dc08d2cffc5e712e671c247c381ed37ff9f9c
Prayogi Setiawan <besutodio@gmail.com>
2025-01-24 22:53:21 +0700

    yogi: integrasi api ke halaman home

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,28 @@
 
 import { Button } from "flowbite-react";
 import Link from "next/link";
+import { useEffect, useState } from "react";
+import { getProducts } from "./api/home";
 
 export default function Home() {
+	const [products, setProducts] = useState({});
+
+	const fetchProducts = async () => {
+		const products = await getProducts();
+		setProducts(products);
+	}
+
+	useEffect(() => {
+		fetchProducts();
+	}, []);
+
+	function formatRupiah(angka: number): string {
+		if (isNaN(angka)) {
+			return '';
+		}
+		return 'Rp ' + angka.toLocaleString('id-ID');
+	}
+
 	return (
 		<div>
 			<section className="bg-primary-100 rounded-2xl mt-5">
@@ -50,30 +70,32 @@ export default function Home() {
 						</div>
 					</div>
 					<div className="mb-4 grid gap-4 sm:grid-cols-2 md:mb-8 lg:grid-cols-3 xl:grid-cols-4">
-						<div className="rounded-lg border border-gray-200 bg-white p-6 shadow-sm dark:border-gray-700 dark:bg-gray-800">
-							<div className="h-56 w-full">
-								<Link href=''>
-									<img className="mx-auto h-full" src='' alt="" />
-								</Link>
-							</div>
-							<div className="pt-6">
-								<Link href='' className="text-lg font-semibold leading-tight text-gray-900 hover:underline dark:text-white">Gelang Dari Kayu</Link>
+						{Object.values(products)?.map((product: any) => (
+							<div key={product.id} className="rounded-lg border border-gray-200 bg-white p-6 shadow-sm dark:border-gray-700 dark:bg-gray-800">
+								<div className="h-56 w-full">
+									<Link href={`/products/${product.slug}`}>
+										<img className="mx-auto h-full" src={`${product.image}`} alt="" />
+									</Link>
+								</div>
+								<div className="pt-6">
+									<Link href={`/products/${product.slug}`} className="text-lg font-semibold leading-tight text-gray-900 hover:underline dark:text-white">{product.name}</Link>
 
-								<ul className="mt-2 flex items-center gap-4">
-									<li className="flex items-center gap-2">
-										<svg className="h-4 w-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-											<path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13.5 8H4m0-2v13a1 1 0 0 0 1 1h14a1 1 0 0 0 1-1V9a1 1 0 0 0-1-1h-5.032a1 
-												1 0 0 1-.768-.36l-1.9-2.28a1 1 0 0 0-.768-.36H5a1 1 0 0 0-1 1Z"/>
-										</svg>
-										<p className="text-sm font-medium text-gray-500 dark:text-gray-400">Kerajinan Tangan</p>
-									</li>
-								</ul>
+									<ul className="mt-2 flex items-center gap-4">
+										<li className="flex items-center gap-2">
+											<svg className="h-4 w-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+												<path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13.5 8H4m0-2v13a1 1 0 0 0 1 1h14a1 1 0 0 0 1-1V9a1 1 0 0 0-1-1h-5.032a1 
+													1 0 0 1-.768-.36l-1.9-2.28a1 1 0 0 0-.768-.36H5a1 1 0 0 0-1 1Z"/>
+											</svg>
+											<p className="text-sm font-medium text-gray-500 dark:text-gray-400">{product.category.name}</p>
+										</li>
+									</ul>
 
-								<div className="mt-4 flex items-center justify-between gap-4">
-									<p className="text-xl font-bold leading-tight">Rp 50.000</p>
+									<div className="mt-4 flex items-center justify-between gap-4">
+										<p className="text-xl font-bold leading-tight">{formatRupiah(product.price)}</p>
+									</div>
 								</div>
 							</div>
-						</div>
+						))}
 					</div>
 					<div className="text-center flex justify-center">
 						<Button as={Link} href="/products" className="hover:bg-gray-100" color="light">Show more</Button>
